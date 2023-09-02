bcf8e9c306e648af98cd3e1daebbee22ada156c3
db9crt <116734267+db9crt@users.noreply.github.com>
2023-09-02 10:39:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -209,8 +209,13 @@ resource "azurerm_app_service_source_control" "sourcecontrol" {
   }
   depends_on = [ azurerm_source_control_token.dobble_token1 ]
 }
+import {
+   id = "/subscriptions/27797fca-63b0-46fd-87c7-0757c81e041a/resourceGroups/project_rg/providers/Microsoft.Web/sites/MyNodeJsAppproject"
+   to = azurerm_source_control_token.dobble_token1
+ }
   resource "azurerm_source_control_token" "dobble_token1" {
   type  = "GitHub"
   token = "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"
 }
+
 
