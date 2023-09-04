61d2a86a7339985a4396c8cc30aedeebf107c1e8
db9crt <116734267+db9crt@users.noreply.github.com>
2023-09-04 17:46:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,8 +33,8 @@ provider "azurerm" {
 }
 
 data "azurerm_client_config" "current" {}
-resource "azurerm_key_vault" "keydona" {
-  name                        = "keydona"
+resource "azurerm_key_vault" "littledona" {
+  name                        = "littledona"
   location                    = azurerm_resource_group.project_rg.location
   resource_group_name         = azurerm_resource_group.project_rg.name
   enabled_for_disk_encryption = true
@@ -70,7 +70,7 @@ resource "azurerm_key_vault" "keydona" {
 }
 
 resource "azurerm_key_vault_access_policy" "project-principalkey" {
-  key_vault_id = azurerm_key_vault.keydona.id
+  key_vault_id = azurerm_key_vault.littledona.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = azurerm_windows_web_app.MyNodeJsApp.identity.0.principal_id
 
@@ -87,7 +87,7 @@ data "azuread_user" "user" {
   user_principal_name  = "db9crt_bolton.ac.uk#EXT#@db9crt.onmicrosoft.com"
 }
 resource "azurerm_key_vault_access_policy" "user-principalkey" {
-  key_vault_id = azurerm_key_vault.keydona.id
+  key_vault_id = azurerm_key_vault.littledona.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azuread_user.user.object_id
 
@@ -149,7 +149,7 @@ data "azurerm_cosmosdb_account" "projectcosmosdbacct"{
 resource "azurerm_key_vault_secret" "projectsecretnewer" {
   name         = "projectsecretnewer"
   value        = azurerm_cosmosdb_account.projectcosmosdbacct.connection_strings[0]
-  key_vault_id = azurerm_key_vault.keydona.id
+  key_vault_id = azurerm_key_vault.littledona.id
 }
 resource "azurerm_cosmosdb_mongo_database" "project_cosmosdb" {
   name                = "project_cosmosdb"
@@ -175,7 +175,7 @@ resource "azurerm_windows_web_app" "MyNodeJsApp" {
     "WEBSITE_NODE_DEFAULT_VERSION" = "~16"
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
     "DATABASE_NAME" = azurerm_cosmosdb_mongo_database.project_cosmosdb.name
-    "DATABASE_URL" ="@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.keydona.vault_uri}secrets/${azurerm_key_vault_secret.projectsecretnewer.name}/${azurerm_key_vault_secret.projectsecretnewer.version})"  //"@Microsoft.KeyVault(SecretUri=https://keydonaectdbstring.vault.azure.net/secrets/projectsecret)"//azurerm_cosmosdb_account.projectcosmosdbacct.connection_strings[0]  
+    "DATABASE_URL" ="@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.littledona.vault_uri}secrets/${azurerm_key_vault_secret.projectsecretnewer.name}/${azurerm_key_vault_secret.projectsecretnewer.version})"  //"@Microsoft.KeyVault(SecretUri=https://littledonaectdbstring.vault.azure.net/secrets/projectsecret)"//azurerm_cosmosdb_account.projectcosmosdbacct.connection_strings[0]  
   }
   identity {
     type = "SystemAssigned"
@@ -203,7 +203,7 @@ resource "azurerm_cosmosdb_mongo_collection" "projectmongoCollection" {
 }
 
 
-/*resource "azurerm_app_service_source_control" "sourcecontrol" {
+resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id   = azurerm_windows_web_app.MyNodeJsApp.id
   repo_url = "https://github.com/db9crt/node-mongodb-app"
   branch   = "main"
@@ -216,20 +216,20 @@ resource "azurerm_cosmosdb_mongo_collection" "projectmongoCollection" {
     }
   }
   depends_on = [ azurerm_source_control_token.dobble_token1 ]
-}*/
+}
 /*import {
    id = "/subscriptions/27797fca-63b0-46fd-87c7-0757c81e041a/resourceGroups/project_rg/providers/Microsoft.Web/sites/MyNodeJsAppproject"
    to = azurerm_source_control_token.dobble_token1
  }*/
-/*  resource "azurerm_source_control_token" "dobble_token1" {
+  resource "azurerm_source_control_token" "dobble_token1" {
   type  = "GitHub"
   token = "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"
-}*/
-/*resource "azurerm_ssh_public_key" "sshkey" {
+}
+resource "azurerm_ssh_public_key" "sshkey" {
   name                = "sshkey"
   resource_group_name = "project_rg"
   location = "Uk South"
   public_key = trimspace(file("/c/Users/annie/.ssh/id_rsa.pub"))
  
-}*/
+}
 
